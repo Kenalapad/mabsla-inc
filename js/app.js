5609b74a6096b8da01628048040c79588a5ed37e
Kenneth Lester Alapad <89431616+Kenalapad@users.noreply.github.com>
2024-06-18 22:50:37 +0800

    Folder Assets

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// NavBar onhover change background-color to #8DECB4/LightGreen
+$(document).ready(function(){
+    $(".dropdown-item").hover(function(){
+        $(this).css("background-color", "#8DECB4");
+        }, function(){
+        $(this).css("background-color", "white");
+    });
+});
+// end NavBar onhover change background-color to #8DECB4/LightGreen
